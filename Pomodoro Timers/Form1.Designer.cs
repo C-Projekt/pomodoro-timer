926399cf035a1997755b6d127ceba2f86093003a
Ng Cheuk Yin <cheukyin699@yahoo.com>
2015-10-24 09:52:11 -0700

    Initial Commit

---
+++ Pomodoro Timers/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Pomodoro_Timers
+{
+    partial class pomoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pomoForm));
+            this.pomoTimer = new System.Windows.Forms.Timer(this.components);
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pomoVal = new System.Windows.Forms.NumericUpDown();
+            this.breakVal = new System.Windows.Forms.NumericUpDown();
+            this.startBt = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pomoVal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.breakVal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pomoTimer
+            // 
+            this.pomoTimer.Tick += new System.EventHandler(this.pomoTimer_Tick);
+            // 
+            // progBar
+            // 
+            this.progBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progBar.Location = new System.Drawing.Point(13, 13);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(259, 23);
+            this.progBar.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 43);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(259, 174);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(5, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "POMODORO";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(-4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "POMODORO";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(3, 2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "BREAK";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pomoVal);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(118, 203);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.breakVal);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(129, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(130, 205);
+            this.panel2.TabIndex = 1;
+            // 
+            // pomoVal
+            // 
+            this.pomoVal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pomoVal.Location = new System.Drawing.Point(3, 22);
+            this.pomoVal.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.pomoVal.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.pomoVal.Name = "pomoVal";
+            this.pomoVal.Size = new System.Drawing.Size(111, 20);
+            this.pomoVal.TabIndex = 1;
+            this.pomoVal.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.pomoVal.ValueChanged += new System.EventHandler(this.pomoVal_ValueChanged);
+            // 
+            // breakVal
+            // 
+            this.breakVal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.breakVal.Location = new System.Drawing.Point(7, 24);
+            this.breakVal.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.breakVal.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.breakVal.Name = "breakVal";
+            this.breakVal.Size = new System.Drawing.Size(120, 20);
+            this.breakVal.TabIndex = 2;
+            this.breakVal.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.breakVal.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // startBt
+            // 
+            this.startBt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startBt.Location = new System.Drawing.Point(13, 224);
+            this.startBt.Name = "startBt";
+            this.startBt.Size = new System.Drawing.Size(259, 34);
+            this.startBt.TabIndex = 2;
+            this.startBt.Text = "Start";
+            this.startBt.UseVisualStyleBackColor = true;
+            this.startBt.Click += new System.EventHandler(this.startBt_Click);
+            // 
+            // pomoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.startBt);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.progBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "pomoForm";
+            this.Text = "Pomodoro Timer";
+            this.Load += new System.EventHandler(this.pomoForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pomoVal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.breakVal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer pomoTimer;
+        private System.Windows.Forms.ProgressBar progBar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NumericUpDown pomoVal;
+        private System.Windows.Forms.NumericUpDown breakVal;
+        private System.Windows.Forms.Button startBt;
+    }
+}
+
---
+++ Pomodoro Timers/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pomodoro_Timers
+{
+    public partial class pomoForm : Form
+    {
+        decimal pomoTime, breakTime, timeElapsed;
+        bool isPomo = true;
+
+        private void startBt_Click(object sender, EventArgs e)
+        {
+            if (pomoTimer.Enabled) return;
+            pomoTime = pomoVal.Value * 60; breakTime = breakVal.Value * 60;
+            timeElapsed = 0;
+
+            pomoTimer.Start();
+
+            ((Button)sender).Enabled = false;
+        }
+
+        private void pomoTimer_Tick(object sender, EventArgs e)
+        {
+            timeElapsed += (decimal)(((Timer)sender).Interval) / 1000;
+
+            if (timeElapsed >= (isPomo ? pomoTime : breakTime))
+            {
+                timeElapsed = 0;
+                isPomo = !isPomo;
+                this.Text = (isPomo ? "Pomodoro Timer" : "Break Timer!");
+
+                // Play a system sound
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+
+            // Update progressbar display
+            progBar.Value = (int)(timeElapsed / (isPomo ? pomoTime : breakTime) * 100);
+        }
+
+        private void pomoVal_ValueChanged(object sender, EventArgs e)
+        {
+            pomoTime = ((NumericUpDown)sender).Value * 60;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            breakTime = ((NumericUpDown)sender).Value * 60;
+        }
+
+        public pomoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pomoForm_Load(object sender, EventArgs e)
+        {
+            pomoTime = pomoVal.Value * 60; breakTime = breakVal.Value * 60;
+            timeElapsed = pomoTime;
+        }
+    }
+}
